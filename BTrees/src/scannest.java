3d4e11539122311cb1adbeb659b119e61ca7aeda
Mason Humpherys <masonhumpherys@u.boisestate.edu>
2019-04-22 13:01:09 -0600

    Update scannest.java

--- BTrees/src/scannest.java
+++ BTrees/src/scannest.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 
 public class scannest{
 	
 	private Scanner scan;
+	private int subLength;
 
-	public scannest(Scanner s) {
+	public scannest(Scanner s, int len) {
 		scan = s;
+		subLength = len;
 	}
 	
 	

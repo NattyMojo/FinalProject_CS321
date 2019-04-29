a254222ea76ea1202c28c62774d35890635dad20
NattyMojo <zachary.luciano@gmail.com>
2019-04-29 17:44:04 -0600

    Added BTreeNode as an inner class of BTree

--- BTrees/src/BTree.java
+++ BTrees/src/BTree.java
@@ -1,4 +1,17 @@
 
 public class BTree {
 
+	public class BTreeNode {
+		public String[] keys;
+		public BTreeNode[] children;
+		public int degree;
+		
+		public BTreeNode(int t) {
+			keys = new String[(2*t)-1];
+			children = new BTreeNode[(2*t)]
+			degree = t;
+		}
+		
+	}
+	
 }

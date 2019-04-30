3ee3e210b11256bf7e946b88a49c8789e17fb66d
NattyMojo <zachary.luciano@gmail.com>
2019-04-29 18:36:37 -0600

    Have added in leftChild method

--- BTrees/src/BTree.java
+++ BTrees/src/BTree.java
@@ -8,8 +8,33 @@ public class BTree {
 		
 		public BTreeNode(int t) {
 			keys = new String[(2*t)-1];
-			children = new BTreeNode[(2*t)]
+			children = new BTreeNode[(2*t)];
 			degree = t;
+		}
+		
+		//Finds key based on given substring and returns position in array or -1 if doesn't exist 
+		public int findKey(String sub) {
+			if(keys.length < 1) {
+				return -1;
+			}
+			int i = 0;
+			String currentKey = keys[i];
+			while(!(sub.equals(currentKey))) {
+				currentKey = keys[i++];
+			}
+			if(currentKey == null) {
+				return -1;
+			}
+			else
+				return i;
+		}
+		
+		public BTreeNode leftChild(String sub) {
+			int index = findKey(sub);
+			if(index == -1) {
+				return null;
+			}
+			return children[index+1];					//TODO: I don't think I did this right please check	
 		}
 		
 	}

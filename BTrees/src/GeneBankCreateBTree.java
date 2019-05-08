e26b0fe704fa153961cc7982ad0ef1472a42798d
byoshino <49956949+byoshino@users.noreply.github.com>
2019-05-08 12:47:25 -0600

    Add files via upload

--- BTrees/src/GeneBankCreateBTree.java
+++ BTrees/src/GeneBankCreateBTree.java
@@ -16,7 +16,6 @@ public class GeneBankCreateBTree {
 	public static final int DEBUG_MAX = 1;
 
 	public static void main(String[] args) throws IOException {
-		boolean checkCache = false;
 		if (args.length < 4) {
 			badUsage();
 		}
@@ -25,111 +24,19 @@ public class GeneBankCreateBTree {
 		
 		//cache
 		int ca = Integer.parseInt(args[0]);
-		checkCache = tryCache(ca);
+		tryCache(ca);
 		//degree
 		int deg = Integer.parseInt(args[1]);
-		int BTreeDegree = getDegree(deg);
+		getDegree(deg);
 		//sequence length
 		int len = Integer.parseInt(args[3]);
-		int sequenceLength = seqLeng(len);
-
+		seqLeng(len);
+		//debug level
+		int dlevel = Integer.parseInt(args[4]);
+		getDebLev(dlevel);
+			
 		
-		
-		
-		// cache and debug level
-		int debugLevel = 0;
-
-		if (!checkCache || args.length > 4) {
-			try {
-				int dlevel = Integer.parseInt(args[4]);
-				if (dlevel < 0 || dlevel > DEBUG_MAX) badUsage();
-				else debugLevel = dlevel;
-			} catch (NumberFormatException e) {
-					badUsage();
-			}
-		}
-		
-		
-		
-		
-		// genebank file
-		File gbk = new File(args[2]);
-
-		BufferedReader in = null;
-		try {
-			in = new BufferedReader(new FileReader(gbk));
-		} catch (FileNotFoundException e) {
-			System.err.println("File not found: " + gbk.getPath());
-		}
-		String BTreeFile = (gbk + ".btree.data." + sequenceLength + "." + BTreeDegree);
-		BTree tree = new BTree(BTreeDegree, BTreeFile, checkCache, cacheSize);
-
-		String line = null;
-		line = in.readLine().toLowerCase().trim();
-		boolean inSequence = false;
-		int sequencePosition = 0;
-		int charPosition = 0;
-		long sequence = 0L;
-
-		while (line != null) { 
-			if (inSequence) {
-				if (line.startsWith("//")) { 
-					inSequence = false;
-					sequence = 0;
-					sequencePosition = 0;
-				} else {
-					while (charPosition < line.length()) {
-						char c = line.charAt(charPosition++);
-						switch (c) {
-						case 'a':
-							sequence = ((sequence<<2) | BASE_A);
-							if (sequencePosition < sequenceLength) sequencePosition++;
-							break;
-						case 't':
-							sequence = ((sequence<<2) | BASE_T);
-							if (sequencePosition < sequenceLength) sequencePosition++;
-							break;
-						case 'c':
-							sequence = ((sequence<<2) | BASE_C);
-							if (sequencePosition < sequenceLength) sequencePosition++;
-							break;
-						case 'g':
-							sequence = ((sequence<<2) | BASE_G);
-							if (sequencePosition < sequenceLength) sequencePosition++;
-							break;
-						case 'n':
-							sequencePosition = 0;
-							sequence = 0;
-							continue;
-						default: //none of the above
-							continue;
-						}
-						if (sequencePosition >= sequenceLength) {
-							tree.BTreeInsert(sequence & (~(0xffffffffffffffffL<<(sequenceLength<<1))));
-						}
-					}
-				}
-			} else if (line.startsWith("ORIGIN")) {
-				inSequence = true;
-			}
-			line = in.readLine();
-			charPosition = 0;
-		}
-
-		if (debugLevel > 0) {
-			File dumpFile = new File("dump");
-			dumpFile.delete();
-			dumpFile.createNewFile();
-			PrintWriter writer = new PrintWriter(dumpFile);
-			tree.inOrderPrintToWriter(tree.getRoot(),writer,sequenceLength);
-			writer.close();
-		}
-		System.out.println("done");
-		if (checkCache) tree.flushCache();
-		in.close();
 	}
-	
-	
 	
 	
 	
@@ -174,7 +81,15 @@ public class GeneBankCreateBTree {
 	} // end get degree method
 	
 	
-	
+	public static int getOptimalDegree(){
+		double opt;
+		opt = 4096;
+		opt = opt+12;
+		opt = opt-4;
+		opt = opt-5;
+		opt /= (2 * (16));
+		return (int) Math.floor(opt);
+	}
 	
 	
 	public static int seqLeng(int len) {
@@ -195,21 +110,31 @@ public class GeneBankCreateBTree {
 	
 	
 	
+	
+	
+	public static int getDebLev(int dlevel) {
+		int debugLevel = 0;
+		try {
+			if (dlevel < 0 || dlevel > DEBUG_MAX) {
+				badUsage();
+				return debugLevel;
+			}
+			else {
+				debugLevel = dlevel;
+				return debugLevel;
+			}
+		} catch (NumberFormatException e) {
+				badUsage();
+		}
+		return debugLevel;
+	}//end debug level
+	
+	
+	
 
-                      
-	public static int getOptimalDegree(){
-		double optimum;
-		int sizeOfPointer = 4;
-		int sizeOfObject = 12;
-		int sizeOfMetadata = 5;
-		optimum = 4096;
-		optimum += sizeOfObject;
-		optimum -= sizeOfPointer;
-		optimum -= sizeOfMetadata;
-		optimum /= (2 * (sizeOfObject + sizeOfPointer));
-		return (int) Math.floor(optimum);
-	}
+                     
 	
 	public static void badUsage() {}
 	
 }
+	
